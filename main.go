f1e47f315945803d810afba02afae3d6f98f569d
Tonmoy Chiran <Tonmoy Chiran>
2021-07-13 15:43:31 +0600

    Learned about keyboad input

--- main.go
+++ main.go
@@ -1,12 +1,35 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strconv"
 	"strings"
 )
 
 func main() {
-	//go-lang strings
-	string := strings.Join([]string{"Hello", "World"}, ",")
-	fmt.Println(string)
+	//go-lang keyboard input
+	// inputReader := bufio.NewReader(os.Stdin)
+	// fmt.Print("Enter your name: ")
+	// name, _ := inputReader.ReadString('\n')
+	// fmt.Print("Your name is: " + name)
+	inputReader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter a number: ")
+	number, _ := inputReader.ReadString('\n')
+
+	number = strings.Replace(number, "\n", "", -1)
+
+	num, e := strconv.Atoi(number)
+
+	if e != nil {
+		fmt.Println("Conversion error: ", number)
+	}
+
+	if num >= 1 && num <= 10 {
+		fmt.Println("Correct")
+	} else {
+		fmt.Println("Num is not in range")
+	}
+
 }

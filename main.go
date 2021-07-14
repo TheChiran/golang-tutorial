9ff94ca19b3a6e9ef7f1f5c1565e0746c64ebb2e
Tonmoy Chiran <Tonmoy Chiran>
2021-07-14 14:39:56 +0600

    Learned about range

--- main.go
+++ main.go
@@ -3,24 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	// for loops
-	for x := 0; x < 4; x++ {
-		fmt.Printf("Iteration x: %d\n", x)
+	// range
+	numbers := []int{1, 2, 3, 4, 5, 6}
+	// _ is used to ignore index
+	for index, number := range numbers {
+		fmt.Print(index, ") ", number, "\n")
 	}
-	loopArray()
-	countFromOneToTen()
-}
-func loopArray() {
-	array := []int{1, 2, 3, 4, 5, 6}
-	fmt.Println("Loop Array")
-	for i := 0; i < len(array); i++ {
-		fmt.Println("Number : ", array[i])
-	}
-}
 
-func countFromOneToTen() {
-	fmt.Println("Count From one to ten")
-	for i := 0; i < 10; i++ {
-		fmt.Println("Count : ", (i + 1))
-	}
 }

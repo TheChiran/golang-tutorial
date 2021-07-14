9b64412e9c1004c6cec3cd1a624a1059c43be38a
Tonmoy Chiran <Tonmoy Chiran>
2021-07-14 14:29:25 +0600

    Learned about for loops in golang

--- main.go
+++ main.go
@@ -3,12 +3,24 @@ package main
 import "fmt"
 
 func main() {
-	// define array
-	a := [5]int{1, 2, 3, 4, 5}
+	// for loops
+	for x := 0; x < 4; x++ {
+		fmt.Printf("Iteration x: %d\n", x)
+	}
+	loopArray()
+	countFromOneToTen()
+}
+func loopArray() {
+	array := []int{1, 2, 3, 4, 5, 6}
+	fmt.Println("Loop Array")
+	for i := 0; i < len(array); i++ {
+		fmt.Println("Number : ", array[i])
+	}
+}
 
-	// create slice from array
-	t := a[1:3:5]
-
-	// output slice
-	fmt.Println(t)
+func countFromOneToTen() {
+	fmt.Println("Count From one to ten")
+	for i := 0; i < 10; i++ {
+		fmt.Println("Count : ", (i + 1))
+	}
 }

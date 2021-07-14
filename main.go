5d638538f021a8b9c7f3cb01f4cd592896d159ea
Tonmoy Chiran <Tonmoy Chiran>
2021-07-14 12:01:53 +0600

    Learned about go slices

--- main.go
+++ main.go
@@ -2,22 +2,13 @@ package main
 
 import "fmt"
 
-func arrayExample() {
-	fmt.Println("Array example")
-	var array = []int64{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-	fmt.Println(array)
-}
 func main() {
-	//arrays
-	//in golang array value is set to default 0
-	var array = new([5]int)
-	fmt.Println(array[2])
-	fmt.Println(array[3])
-	arrayExample()
-	arrayString()
-}
-func arrayString() {
-	var string = []string{"T", "O", "N", "M", "O", "Y"}
-	fmt.Println("Array string example")
-	fmt.Println(string)
+	// define array
+	a := [5]int{1, 2, 3, 4, 5}
+
+	// create slice from array
+	t := a[1:3:5]
+
+	// output slice
+	fmt.Println(t)
 }

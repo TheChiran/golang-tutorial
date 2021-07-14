1223126e3aa4855267f4b0a12eeaf2765c9d15fa
Tonmoy Chiran <Tonmoy Chiran>
2021-07-14 11:54:50 +0600

    Learned about arrays in golang

--- main.go
+++ main.go
@@ -2,17 +2,22 @@ package main
 
 import "fmt"
 
-//when a variable is assign insides a function
-//that is local variable
-//when that variable is assigned outside of function
-//that is global
-var x = 7 //local variables
-
-func example() {
-	fmt.Println(x)
+func arrayExample() {
+	fmt.Println("Array example")
+	var array = []int64{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	fmt.Println(array)
 }
 func main() {
-	//scopes
-	fmt.Println(x)
-	example()
+	//arrays
+	//in golang array value is set to default 0
+	var array = new([5]int)
+	fmt.Println(array[2])
+	fmt.Println(array[3])
+	arrayExample()
+	arrayString()
+}
+func arrayString() {
+	var string = []string{"T", "O", "N", "M", "O", "Y"}
+	fmt.Println("Array string example")
+	fmt.Println(string)
 }

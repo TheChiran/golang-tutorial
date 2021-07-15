2388c5431d6e8b93b014f4652a4a19ac13d03580
Tonmoy Chiran <Tonmoy Chiran>
2021-07-15 20:45:12 +0600

    Learned about normal map

--- main.go
+++ main.go
@@ -4,21 +4,12 @@ import (
 	"fmt"
 )
 
-type House struct {
-	rooms int
-	price int32
-	city  string
-}
-
 func main() {
-	// struct
-	var house House
-	house.rooms = 4
-	house.price = 36550
-	house.city = "Dhaka"
+	// golang map
+	elements := make(map[string]string)
+	elements["O"] = "Oxygen"
+	elements["Ca"] = "Calcium"
+	elements["C"] = "Carbon"
 
-	fmt.Printf("House.rooms = %d \n", house.rooms)
-	fmt.Printf("House.price = %d \n", house.price)
-	fmt.Printf("House.city = %s \n", house.city)
-
+	fmt.Println(elements["C"])
 }

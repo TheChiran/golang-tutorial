b09b7f1065b0e1bbf15e5960fe399fcd00f57ec7
Tonmoy Chiran <Tonmoy Chiran>
2021-07-15 23:32:10 +0600

    Go lang pointers

--- main.go
+++ main.go
@@ -1,18 +1,20 @@
 package main
 
-import (
-	"fmt"
-	"math/rand"
-	"time"
-)
+import "fmt"
 
-func random(min int, max int) int {
-	return rand.Intn(max-min) + min
-}
 func main() {
-	// golang random numbers
-	rand.Seed(time.Now().UnixNano())
-	randomNum := random(1, 6)
-	fmt.Println("Random Number: ", randomNum)
+	//pointers
+	// variable
+	var x int = 5
 
+	// create pointer
+	var ipointer *int
+
+	// store the address of x in pointer variable
+	ipointer = &x
+
+	// display info
+	fmt.Printf("Memory address of variable x: %x\n", &x)
+	fmt.Printf("Memory address stored in ipointer variable: %x\n", ipointer)
+	fmt.Printf("Contents of *ipointer variable: %d\n", *ipointer)
 }

b5f46b735f48cea4f36bdba55812d0fa08d56907
Tonmoy Chiran <Tonmoy Chiran>
2021-07-22 23:53:02 +0600

    Channel direction->sendonly and recieve only

--- main.go
+++ main.go
@@ -1,28 +1,17 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
+import "fmt"
 
-func task(done chan bool) {
-	fmt.Print("Task 1 (goroutine) running...")
-	time.Sleep(time.Second)
-	fmt.Println("done")
-
-	done <- true
+func recieveOnly(c <-chan string) {
+	fmt.Println(<-c)
 }
-
-func task2() {
-	fmt.Println("Task 2 (goroutine)")
+func sendOnly(c chan<- string) {
+	c <- "Send only channel"
 }
-
 func main() {
-	done := make(chan bool, 1)
-	go task(done)
-
-	if <-done {
-		go task2()
-		fmt.Scanln()
-	}
+	c := make(chan string, 1)
+	c <- "Recieve Only Channel"
+	recieveOnly(c)
+	sendOnly(c)
+	fmt.Println(<-c)
 }

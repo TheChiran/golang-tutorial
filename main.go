1ee697bc85a76ff42c6208e61f2e83e493ad5c41
Tonmoy Chiran <Tonmoy Chiran>
2021-07-22 23:57:43 +0600

    Channel select

--- main.go
+++ main.go
@@ -1,17 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
-func recieveOnly(c <-chan string) {
-	fmt.Println(<-c)
+func f1(c chan string) {
+	for {
+		time.Sleep(1 * time.Second)
+		c <- "1"
+	}
 }
-func sendOnly(c chan<- string) {
-	c <- "Send only channel"
+
+func f2(c chan string) {
+	for {
+		time.Sleep(1 * time.Second)
+		c <- "2"
+	}
 }
+
 func main() {
-	c := make(chan string, 1)
-	c <- "Recieve Only Channel"
-	recieveOnly(c)
-	sendOnly(c)
-	fmt.Println(<-c)
+	c1 := make(chan string)
+	c2 := make(chan string)
+
+	go f1(c1)
+	go f2(c2)
+
+	for {
+		select {
+		case msg1 := <-c1:
+			fmt.Println(msg1)
+		case msg2 := <-c2:
+			fmt.Println(msg2)
+		}
+	}
+	fmt.Println("Goroutines finished.")
 }

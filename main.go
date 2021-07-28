8c0f696421eb3012cdc50117b2c3613c208a7c8e
Tonmoy Chiran <Tonmoy Chiran>
2021-07-28 10:23:48 +0600

    go timers

--- main.go
+++ main.go
@@ -1,15 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
-	channel := make(chan int, 5)
-	channel <- 5
-	channel <- 3
-	channel <- 9
-	close(channel)
+	t1 := time.NewTimer(time.Second)
+	go func() {
+		<-t1.C
+		fmt.Println("Timer expired")
+	}()
 
-	for element := range channel {
-		fmt.Println(element)
+	fmt.Scanln()
+	stop := t1.Stop()
+	if stop {
+		fmt.Println("Timer stopped")
 	}
 }

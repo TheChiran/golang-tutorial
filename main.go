ac0ce3be92cdbddf7149a806dd5d4280315a226f
Tonmoy Chiran <Tonmoy Chiran>
2021-07-30 14:20:10 +0600

    read json file

--- main.go
+++ main.go
@@ -3,24 +3,36 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 )
 
-type user struct {
-	Username string
-	Password string
-}
-
 func main() {
-	var jsoninput = []byte("[{\"Username\":\"debora\",\"Password\":\"123456\"},{\"Username\":\"bob\",\"Password\":\"42\"},{\"Username\":\"sandra\",\"Password\":\"33\"}]")
-	var users []user
-	err := json.Unmarshal(jsoninput, &users)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Println(users)
 
-	for _, user := range users {
-		fmt.Println("->", user.Username)
+	// read file
+	data, err := ioutil.ReadFile("./example.json")
+	if err != nil {
+		fmt.Print(err)
 	}
+
+	// define data structure
+	type DayPrice struct {
+		USD float32
+		EUR float32
+		GBP float32
+	}
+
+	// json data
+	var obj DayPrice
+
+	// unmarshall it
+	err = json.Unmarshal(data, &obj)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+
+	// can access using struct now
+	fmt.Printf("USD : %.2f\n", obj.USD)
+	fmt.Printf("EUR : %.2f\n", obj.EUR)
+	fmt.Printf("GBP : %.2f\n", obj.GBP)
+
 }
